0e3942760d4ab88fe416347105528f72232a5306
Sharath <mailtosharath1305@gmail.com>
2025-01-24 23:53:04 +0530

    Initial Commit

---
+++ EmployeeManagementSystem.Tests/ControllerTests'/EmployeeControllerTests.cs
@@ -0,0 +1,74 @@
+﻿using EmployeeManagementSystem.Controllers;
+using EmployeeManagementSystem.Models;
+using EmployeeManagementSystem.Repositories;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace EmployeeManagementSystem.Tests.Controllers
+{
+    [TestFixture]
+    public class EmployeeControllerTests
+    {
+        private Mock<IEmployeeRepository> _mock;
+        private EmployeeManagementController _controller;
+
+        [SetUp]
+        public void Setup()
+        {
+            _mock = new Mock<IEmployeeRepository>();
+            _controller = new EmployeeManagementController(_mock.Object);
+        }
+
+        [Test]
+        public async Task GetAllEmployees_Admin_ShouldReturnAllEmployees()
+        {
+            // Arrange
+            var employees = new List<Employee>
+            {
+                new Employee {  Id = 1, Name = "Sharath", Email = "sharath@trimble.com", Position = "Developer", Role = "Employee" },
+                new Employee { Id = 2, Name = "David", Email = "david@trimble.com", Position = "Manager", Role = "Admin" }
+            };
+            _mock.Setup(repo => repo.GetAllEmpployees()).ReturnsAsync(employees);
+
+            // Act
+            var result = await _controller.GetAllEmployees("Admin") as OkObjectResult;
+
+            // Assert
+            result.Should().NotBeNull();
+            result.Value.As<IEnumerable<Employee>>().Should().HaveCount(2);
+        }
+
+        [Test]
+        public async Task GetEmployeeById_Employee_ShouldReturnEmployee()
+        {
+            // Arrange
+            var employee = new Employee { Id = 1, Name = "Sharath", Email = "sharath@trimble.com", Position = "Developer", Role = "Employee" };
+            _mock.Setup(repo => repo.GetEmployeeDetailsById(1)).ReturnsAsync(employee);
+
+            // Act
+            var result = await _controller.GetEmployeeById("Employee", 1) as OkObjectResult;
+
+            // Assert
+            result.Should().NotBeNull();
+            result.Value.As<Employee>().Name.Should().Be("Sharath");
+        }
+
+        [Test]
+        public async Task AddEmployee_Admin_ShouldAddEmployee()
+        {
+            // Arrange
+            var newEmployee = new Employee { Id = 3, Name = "Karthik", Email = "karthik@trimbl.com", Position = "Tester", Role = "Employee" };
+
+            // Act
+            var result = await _controller.AddEmployee("Admin", newEmployee) as CreatedAtActionResult;
+
+            // Assert
+            result.Should().NotBeNull();
+            result.Value.As<Employee>().Name.Should().Be("Karthik");
+        }
+    }
+}
---
+++ EmployeeManagementSystem.Tests/RepositoryTests/EmployeeRepositoryTests.cs
@@ -0,0 +1,86 @@
+﻿using EmployeeManagementSystem.Data;
+using EmployeeManagementSystem.Models;
+using EmployeeManagementSystem.Repositories;
+using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+using NUnit.Framework;
+using System.Threading.Tasks;
+
+namespace EmployeeManagementSystem.Tests.Repositories
+{
+    [TestFixture]
+    public class EmployeeRepositoryTests
+    {
+        private EmployeeDBContext _context;
+        private EmployeeRepository _repository;
+
+        [SetUp]
+        public void Setup()
+        {
+            var options = new DbContextOptionsBuilder<EmployeeDBContext>()
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+                .Options;
+
+            _context = new EmployeeDBContext(options);
+            _repository = new EmployeeRepository(_context);
+
+            // Seed test data
+            _context.Employees.AddRange(
+               new Employee { Id = 1, Name = "Sharath", Email = "sharath@trimble.com", Position = "Developer", Role = "Employee" },
+                new Employee { Id = 2, Name = "David", Email = "david@trimble.com", Position = "Manager", Role = "Admin" }
+            );
+            _context.SaveChanges();
+        }
+        [TearDown]
+        public void TearDown()
+        {
+            // Dispose of the context
+            _context.Dispose();
+        }
+
+        [Test]
+        public async Task GetAllAsync_ShouldReturnAllEmployees()
+        {
+            var employees = await _repository.GetAllEmpployees();
+            employees.Should().HaveCount(2);
+        }
+
+        [Test]
+        public async Task GetByIdAsync_ShouldReturnCorrectEmployee()
+        {
+            var employee = await _repository.GetEmployeeDetailsById(1);
+            employee.Should().NotBeNull();
+            employee.Name.Should().Be("Sharath");
+        }
+
+        [Test]
+        public async Task AddAsync_ShouldAddNewEmployee()
+        {
+            var newEmployee = new Employee { Name = "Karthik", Email = "karthik@trimbl.com", Position = "Tester", Role = "Employee" };
+            await _repository.UpdateEmployeeDetailsById(newEmployee);
+
+            var employees = await _repository.GetAllEmpployees();
+            employees.Should().HaveCount(3);
+        }
+
+        [Test]
+        public async Task UpdateAsync_ShouldUpdateEmployeeDetails()
+        {
+            var employee = await _repository.GetEmployeeDetailsById(1);
+            employee.Position = "Senior Developer";
+
+            await _repository.UpdateEmployeeDetailsById(employee);
+
+            var updatedEmployee = await _repository.GetEmployeeDetailsById(1);
+            updatedEmployee.Position.Should().Be("Senior Developer");
+        }
+
+        [Test]
+        public async Task DeleteAsync_ShouldRemoveEmployee()
+        {
+            await _repository.DeleteEmployeeDetailsById(1);
+            var employees = await _repository.GetAllEmpployees();
+            employees.Should().HaveCount(1);
+        }
+    }
+}
